fd178ada063f6d53316c0aa854e442f1c17c608e
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2021-04-11 22:33:26 -0700

    Add files via upload

---
+++ structure_6/structure_6_1.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#define MAX_DEGREE 101
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+
+
+typedef struct{
+    int degree;
+    float coef[MAX_DEGREE];
+}polynomial;
+
+
+polynomial poly_add1(polynomial A, polynomial B){
+    polynomial c;
+    int Apos = 0, Bpos = 0, Cpos = 0;
+    int degree_a = A.degree;
+    int degree_b = B.degree;
+    c.degree = MAX(A.degree, B.degree);
+    while(Apos <= A.degree && Bpos <= B.degree){
+        if(degree_a > degree_b){
+            c.coef[Cpos++] = A.coef[Apos++];
+            degree_a--;
+        }
+        else if(degree_a == degree_b){
+            c.coef[Cpos++] = A.coef[Apos++] + B.coef[Bpos++];
+            degree_a--; degree_b--;
+        }
+        else{
+            c.coef[Cpos++] = B.coef[Bpos++];
+            degree_b--;
+        }
+    }
+    return c;
+}
+
+void print_poly(polynomial p){
+    for(int i = p.degree; i>0; i--)
+        printf("%3.1fx^%d + ", p.coef[p.degree-i],i);
+    
+    printf("%3.1f \n", p.coef[p.degree]);
+}
+
+int main(void){
+    polynomial a = { 5, {3,6,0,0,0,10}};
+    polynomial b = { 4, {7,0,5,0,1}};
+    polynomial c;
+    print_poly(a);
+    print_poly(b);
+    c = poly_add1(a, b);
+    printf("-------------------------------------\n");
+    print_poly(c);
+    return 0;
+}
+
